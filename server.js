4ecc2b8a0c9ee975cee8b4e116efa69d6d54f2ea
Josiah Kosgei <jtomno90@gmail.com>
2020-02-17 18:18:02 +0100

    express app route configuration

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+
 // Start the app by listening on the default Heroku port
+
 app.listen(process.env.PORT || 8080);
