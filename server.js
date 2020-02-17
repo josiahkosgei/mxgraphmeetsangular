22bb249de1130edbe1dac71c8686b231ea85e74c
Josiah Kosgei <jtomno90@gmail.com>
2020-02-17 17:55:35 +0100

    nodedeamon setup

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-// server.js
+const path = require('path');
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
+
 app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
