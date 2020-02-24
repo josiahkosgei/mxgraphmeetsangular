2475c106715d950122a40679b29aa7e67df2ec7b
Josiah Kosgei <jtomno90@gmail.com>
2020-02-24 14:38:03 +0100

    set app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mxgraphmeetsangular';
+  title = 'mxGraph Angular Playground';
   constructor(
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer
